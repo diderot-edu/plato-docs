24e47d28bdcc80299d9ac0ac6f24debf2f239819
agao2cmu <agao2@andrew.cmu.edu>
2024-03-07 16:53:38 -0800

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,16 +13,16 @@ const config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://plato.github.io',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/plato-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  projectName: 'plato.github.io', // Usually your repo name.
+  projectName: 'plato-docs', // Usually your repo name.
   organizationName: 'diderot-edu',
-  trailingSlash: false,
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
